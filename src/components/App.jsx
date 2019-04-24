9434418bef9f2e8591a0af68ac353d9a0da72b81
Zachpierce15 <zachary.15pierce@gmail.com>
2019-04-23 17:30:49 -0700

    dinner time, only debounce left

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,79 @@
 import VideoList from "./VideoList.js";
-import exampleVideoData from "../data/exampleVideoData.js";
 import VideoPlayer from "./VideoPlayer.js";
+import Search from "./Search.js";
+import YOUTUBE_API_KEY from "../config/youtube.js";
+import exampleVideoData from "../data/exampleVideoData.js"
+
 // import React from 'react';
 class App extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        beingPlayed: props.videos[0],
-        playList: props.videos.slice(1)
+      this.option = {
+        max: 5,
+        key: YOUTUBE_API_KEY,
+        query: "diablo 3"
       }
+      this.state = {
+        beingPlayed: exampleVideoData[0],
+        playList: exampleVideoData,
+        query: ''
+      }
+
+    }
+    componentDidMount() {
+      var callBack = function(data) {
+        this.setState({
+          beingPlayed: data[0],
+          playList: data
+        });
+      }
+      this.props.searchYouTube(this.option, callBack.bind(this));
     }
     playVideo(video) {
-      this.state.playList.push(this.state.beingPlayed);
-      var indexOfVideo = this.state.playList.indexOf(video);
-      this.state.playList.splice(indexOfVideo, 1);
+      // this.state.playList.push(this.state.beingPlayed);
+      // var indexOfVideo = this.state.playList.indexOf(video);
+      // this.state.playList.splice(indexOfVideo, 1);
       this.setState({
         beingPlayed: video
       });
     }
-    render() {
-     return (<div>
-         <nav className="navbar">
-           <div className="col-md-6 offset-md-3">
-             <div><h5><em>search</em> view goes here</h5></div>
-           </div>
-         </nav>
-         <div className="row">
-           <div className="col-md-7">
-             <div><VideoPlayer video={this.state.beingPlayed}/></div>
-           </div>
-           <div className="col-md-5">
-             <div> <VideoList videos={this.state.playList} onClick={this.playVideo.bind(this)}/></div>
-           </div>
-         </div>
-       </div>);
+
+    changeQuery(event) {
+      //When clicked... Changes our query to the input text. 
+      
+      const keptEvent = event;
+      console.log(keptEvent);
+      this.setState({query: keptEvent.target.value});
+      console.log(keptEvent.target.value);
     }
+    startSearch() {
+      this.option.query = this.state.query;
+      console.log(this.option.query);
+      var callBack = function(data) {
+        this.setState({
+          beingPlayed: data[0],
+          playList: data
+        });
+      }
+      this.props.searchYouTube(this.option, callBack.bind(this));
+    }
+    render() {
+        return (<div>
+            <nav className="navbar">
+              <div className="col-md-6 offset-md-3">
+                <div><Search onChange={this.changeQuery.bind(this)} onClick={this.startSearch.bind(this)}/> </div>
+              </div>
+            </nav>
+            <div className="row">
+              <div className="col-md-7">
+                <div><VideoPlayer video={this.state.beingPlayed}/></div>
+              </div>
+              <div className="col-md-5">
+                <div> <VideoList videos={this.state.playList} onClick={this.playVideo.bind(this)}/></div>
+              </div>
+            </div>
+          </div>); 
+      }
 }
 // var App = () => (
 //   <div>
--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@
 import App from './components/App.js';
 import exampleVideoData from "./data/exampleVideoData.js";
 import searchYouTube from "./lib/searchYouTube.js";
-import YOUTUBE_API_KEY from "./config/youtube.js";
-var option = {
-    max: 5,
-    key: YOUTUBE_API_KEY,
-    query: "diablo 3"
-}
+
+// var option = {
+//     max: 5,
+//     key: YOUTUBE_API_KEY,
+//     query: "diablo 3"
+// }
 
 
-searchYouTube(option, function(data){
-    var liveData = {};
-    liveData['diablo3'] = data;
-    console.log(liveData);
-    console.log(Object.keys(liveData));
-    
-});
+// searchYouTube(option, function(data){
+//     var liveData = {};
+//     liveData['diablo3'] = data;
+//     console.log(liveData);
+//     console.log(Object.keys(liveData));
+
+// });
 
 
-ReactDOM.render(<App videos={liveData.diablo3}/>, document.getElementById('app'));
+ReactDOM.render(<App searchYouTube={searchYouTube.bind(this)}/>, document.getElementById('app'));
 
